74c2dfb64e05deb1863a91f577fcaeccb53f8f03
JFLagar <juan_x_96@hotmail.com>
2023-01-15 17:35:19 +0100

    Implemented Header buttons

---
+++ Assets/Scripts/HeaderButtons.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeaderButtons : MonoBehaviour
+{
+    public Image buttonImage;
+    public Sprite[] buttonSprite;
+    public RectTransform content;
+    public RectTransform otherContent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (content.gameObject.activeSelf)
+            buttonImage.sprite = buttonSprite[1];
+        else
+            buttonImage.sprite = buttonSprite[0];
+    }
+    public void OpenContent()
+    {
+        content.gameObject.SetActive(true);
+        otherContent.gameObject.SetActive(false);
+    }
+}
